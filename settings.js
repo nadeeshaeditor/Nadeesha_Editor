713439b2dd3db75c06c5a1e3ee1728a64449f07b
nadeeshaeditor <115109078+nadeeshaeditor@users.noreply.github.com>
2022-10-06 00:56:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94772291528'] //ur owner number
+global.ownername = "Nadeesha Editor" //ur owner name
+global.ytname = "YT: Pappala" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94772291528']
+global.ownertag = '94772291528' //ur tag number
+global.botnumber = '94772291528' //Add your Bot Number For Log
+global.botname = 'Nadeesha Editor' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCLNUXtky-Kb9JE4T8eMx9fQ/featured" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCLNUXtky-Kb9JE4T8eMx9fQ/featured" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://www.youtube.com/channel/UCLNUXtky-Kb9JE4T8eMx9fQ/featured'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "Nadeesha Editor" //ur sticker watermark packname
+global.author = "Nadeesha Editor" //ur sticker watermark author
+global.footer = "® Nadeesha Editor"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
